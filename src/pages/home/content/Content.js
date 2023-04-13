a994243dbf70ccdfb2f033f76a0a178210d0e8ec
ashish <ash23581221@gmail.com>
2023-04-13 20:28:27 +0530

    Made the grid responsive

--- src/pages/home/content/Content.js
+++ src/pages/home/content/Content.js
@@ -2,9 +2,9 @@ import VideoCard from "./VideoCard";
 
 const Content = () => {
     return (
-        <div className="mx-4">
-            <div className="mt-6 mx-auto flex flex-wrap gap-y-12 gap-x-4 justify-center max-w-[141rem]">
-                {Array.from({ length: 20 }).map((_, index) => {
+        <div className="mx-auto">
+            <div className="mt-6 grid grid-cols-[repeat(auto-fit,minmax(200px,360px))] gap-y-12 gap-x-4 justify-center max-w-[142rem]">
+                {Array.from({ length: 10 }).map((_, index) => {
                     return <VideoCard key={index}>{index}</VideoCard>;
                 })}
             </div>
