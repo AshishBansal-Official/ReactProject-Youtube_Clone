8d0ef40113d72fe9c24b4fd5ace7a927ddda391e
ashish <ash23581221@gmail.com>
2023-04-19 14:56:00 +0530

    Added lazy loading of images

--- src/pages/home/content/VideoThumbnail.js
+++ src/pages/home/content/VideoThumbnail.js
@@ -1,7 +1,13 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
 const VideoThumbnail = ({ thumbnail, duration }) => {
     return (
         <div className="relative w-full aspect-video rounded-xl">
-            <img src={thumbnail} alt="" className="w-full h-full rounded-xl" />
+            <LazyLoadImage
+                src={thumbnail}
+                alt=""
+                className="w-full h-full rounded-xl"
+            />
             <span className="absolute bottom-2 right-2 bg-app-bg p-1 rounded">
                 {duration}
             </span>
